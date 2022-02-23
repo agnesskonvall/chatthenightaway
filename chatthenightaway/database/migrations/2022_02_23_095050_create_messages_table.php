58dd709e9c7fa4dc309675107cb268df634367db
agnesskonvall <89479983+agnesskonvall@users.noreply.github.com>
2022-02-23 11:10:41 +0100

    database established

--- chatthenightaway/database/migrations/2022_02_23_095050_create_messages_table.php
+++ chatthenightaway/database/migrations/2022_02_23_095050_create_messages_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('messages', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('color');
-            $table->rememberToken();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('content');
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('messages');
     }
 };
