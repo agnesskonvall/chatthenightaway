f61f82c4eff8a594593bc2c7de1b29fdc2b07f70
s0wie <sowieh@gmail.com>
2022-03-11 13:41:37 +0100

    added vue components

--- resources/views/chatroom.blade.php
+++ resources/views/chatroom.blade.php
@@ -1,2 +1,17 @@
 <h1>HELLO WELCOME</h1>
 <a href="/logout">log out</a>
+
+<!-- resources/views/chat.blade.php -->
+@section('content')
+<div class="container">
+    <div class="card">
+        <div class="card-header">Chats</div>
+        <div class="card-body">
+            <chat-messages :messages="messages"></chat-messages>
+        </div>
+        <div class="card-footer">
+            <chat-form v-on:messagesent="addMessage" :user="{{ Auth::user() }}"></chat-form>
+        </div>
+    </div>
+</div>
+@endsection
