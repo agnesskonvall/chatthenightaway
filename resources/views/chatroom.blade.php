043568caa9ed92079b747c28997397522422d9d8
s0wie <sowieh@gmail.com>
2022-03-12 20:59:56 +0100

    Removed all VueJS code and added layout.blade.php for css

--- resources/views/chatroom.blade.php
+++ resources/views/chatroom.blade.php
@@ -1,16 +1,14 @@
+@extends('layout')
+@section('content')
 <h1>HELLO WELCOME</h1>
 <a href="/logout">log out</a>
 
-<!-- resources/views/chat.blade.php -->
-@section('content')
 <div class="container">
     <div class="card">
         <div class="card-header">Chats</div>
         <div class="card-body">
-            <chat-messages :messages="messages"></chat-messages>
         </div>
         <div class="card-footer">
-            <chat-form v-on:messagesent="addMessage" :user="{{ Auth::user() }}"></chat-form>
         </div>
     </div>
 </div>
