4337427d9ed4f47d82546fb7aa81c85110ffa804
s0wie <sowieh@gmail.com>
2022-03-13 13:22:09 +0100

    just added a text field

--- resources/views/chatroom.blade.php
+++ resources/views/chatroom.blade.php
@@ -9,6 +9,12 @@
         <div class="card-body">
         </div>
         <div class="card-footer">
+            <form action="/">
+                <div class="d-flex p-2">
+                    <input class="form-control" type="text" name="" id="" placeholder="Type message here">
+                    <button class="btn btn-primary" type="submit">Send</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
